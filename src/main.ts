0c0ac5d29071626d817653e5068f1dba380c1c90
Francisco J. Abril Álvarez <ffrannabril@gmail.com>
2025-05-16 18:32:14 +0200

    wip

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ var app = express();
 app.get("/api/game/:game", async (req: any, res: any) => {
   let target: string = req.params.game.toLocaleLowerCase();
   //TODO: move this to service and execute Service Scraper only if query param is not null
-  let webSite: string[] = req.query.webSite.split(",");
+  //let webSite: string[] = req.query.webSite.split(",");
   let response: WebSite[] = [];
 
   await new EnebaScraper(cheerio, target).scrap().then((webSite) => {
@@ -29,9 +29,9 @@ app.get("/api/game/:game", async (req: any, res: any) => {
   await new GamivoScraper(cheerio, target).scrap().then((webSite) => {
     response.push(webSite);
   });
-  await new KinguinScraper(cheerio, target).scrap().then((webSite) => {
-    response.push(webSite);
-  });
+  // await new KinguinScraper(cheerio, target).scrap().then((webSite) => {
+  //   response.push(webSite);
+  // });
 
   res.json(response);
 });
